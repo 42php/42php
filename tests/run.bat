b1852b654b7f729c32381fd892b7450ec4813baf
Guillaume Gagnaire <alduro.estallas@gmail.com>
2017-05-06 18:17:30 +0200

    Test running scripts using PHP Unit 5.7.19

---
+++ tests/run.bat
@@ -0,0 +1,1 @@
+@php ../scripts/phar/phpunit-5.7.19.phar --bootstrap init-tests.php ./
---
+++ tests/run.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+# Run PHPUnit
+php ../scripts/phar/phpunit-5.7.19.phar --bootstrap init-tests.php ./
