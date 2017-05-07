ef13cc4456317f80a219c8becd3dab53cc1625dc
Guillaume Gagnaire <alduro.estallas@gmail.com>
2017-05-07 22:32:25 +0200

    Test runners

--- tests/run.bat
+++ tests/run.bat
@@ -1,1 +1,1 @@
-@php ../scripts/phar/phpunit-5.7.19.phar --bootstrap init-tests.php ./
+@php ../scripts/phar/phpunit-5.7.19.phar --bootstrap ./init-tests.php ./
--- tests/run.sh
+++ tests/run.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Run PHPUnit
-php ../scripts/phar/phpunit-5.7.19.phar --bootstrap init-tests.php ./
+php ../scripts/phar/phpunit-5.7.19.phar --bootstrap ./init-tests.php ./
