8b038ec664f9324634ea576d1ac59c3a768c13cd
Guillaume Gagnaire <alduro.estallas@gmail.com>
2017-05-07 10:09:09 +0200

    Added docblock

--- tests/ArrayToolsTest.php
+++ tests/ArrayToolsTest.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @author      Guillaume Gagnaire <contact@42php.com>
+ * @link        https://www.github.com/42php/42php
+ * @license     https://opensource.org/licenses/mit-license.html MIT
+ * @copyright   2015-2017 42php
+ */
 
 use                     PHPUnit\Framework\TestCase;
 
