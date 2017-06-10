e57b82bb0f6fcbf28b226d02ef9d6a99b41913e7
Guillaume Gagnaire <alduro.estallas@gmail.com>
2017-06-10 15:36:54 +0200

    Adding recursiveMerge and getAsArray methods

--- core/Core/ArrayTools.php
+++ core/Core/ArrayTools.php
@@ -23,4 +23,43 @@ class                       ArrayTools {
         if (array() === $arr) return false;
         return array_keys($arr) !== range(0, sizeof($arr) - 1);
     }
+
+    /**
+     * Applies array_merge recursively.
+     *
+     * @param array $arr1       The first array
+     * @param array $arr2       The second array
+     * @param string $exclude   The value to exclude
+     *
+     * @return array            The merged array.
+     */
+    public static function      recursiveMerge($arr1, $arr2, $exclude = '') {
+        $ret = $arr1;
+        if (!is_array($ret))
+            return $arr2;
+        foreach ($arr2 as $k => $v) {
+            if (isset($ret[$k])) {
+                if (is_array($v)) {
+                    $ret[$k] = self::recursiveMerge($ret[$k], $v);
+                } elseif ($v !== $exclude)
+                    $ret[$k] = $v;
+            } else {
+                $ret[$k] = $v;
+            }
+        }
+        return $ret;
+    }
+
+    /**
+     * Returns the value in an array, if $val isn't an array.
+     *
+     * @param mixed $val        The value
+     *
+     * @return array            The array
+     */
+    public static function      getAsArray($val) {
+        if (!is_array($val))
+            $val = [$val];
+        return $val;
+    }
 }
