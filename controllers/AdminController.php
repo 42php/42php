d56bce450751c40665fdaf40a25c023926ec6887
Guillaume Gagnaire <alduro.estallas@gmail.com>
2017-06-17 20:50:15 +0200

    Base vue material app

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -6,11 +6,38 @@
  * @copyright   2015-2017 42php
  */
 
+use Core\Conf;
+use Core\View;
+
 /**
  * Class AdminController
  */
 class                   AdminController extends \Core\Controller {
     public function     display() {
-        return "ok";
+        Conf::set('page.title', _t("Panneau d'administration"));
+
+        /**
+         * Fonts
+         */
+        Conf::append('page.css', 'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,400italic');
+        Conf::append('page.css', 'https://fonts.googleapis.com/icon?family=Material+Icons');
+
+        /**
+         * Vue2 JS
+         */
+        Conf::append('page.js', 'https://unpkg.com/vue@2.3.4');
+
+        /**
+         * Vue Material
+         */
+        Conf::append('page.js', 'https://unpkg.com/vue-material@0.7.1');
+        Conf::append('page.css', 'https://unpkg.com/vue-material@0.7.1/dist/vue-material.css');
+
+        /**
+         * App
+         */
+        Conf::append('page.js', '/js/admin/app.min.js');
+
+        return View::render('admin/app');
     }
 }
