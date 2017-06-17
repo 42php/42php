b7a8e3e2616827f8b18471333f6459ce33675a7e
Guillaume Gagnaire <alduro.estallas@gmail.com>
2017-06-17 19:40:38 +0200

    Base route for admin

---
+++ controllers/AdminController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @author      Guillaume Gagnaire <contact@42php.com>
+ * @link        https://www.github.com/42php/42php
+ * @license     https://opensource.org/licenses/mit-license.html MIT
+ * @copyright   2015-2017 42php
+ */
+
+/**
+ * Class AdminController
+ */
+class                   AdminController extends \Core\Controller {
+    public function     display() {
+
+    }
+}
